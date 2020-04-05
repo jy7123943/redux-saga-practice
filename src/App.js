5a4cc68b317b966c79b967774e2ce1f2652a51a5
juyeon <juyeonh@rainist.com>
2020-04-05 13:52:49 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Header from './components/Header';
 import ImageGrid from './components/ImageGrid';
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <Header />
-                <ImageGrid />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Header />
+        <ImageGrid />
+      </div>
+    );
+  }
 }
 
 export default App;
