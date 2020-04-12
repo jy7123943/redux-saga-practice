72f3e9f815b45a9b7a02398a0d263f673d982543
juyeon <juyeonh@rainist.com>
2020-04-12 11:10:57 +0900

    Change store configuration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import configureStore from './store';
+import createStore from './store';
 
 import Header from './components/Header';
 import ImageGrid from './containers/ImageGrid';
 
-const store = configureStore();
+const store = createStore();
 
 class App extends Component {
   render() {
